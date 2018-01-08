fe21fbb50f81eb2b01edc909a323b0100c66c4b4
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-08 13:59:40 +0900

    change: add debug code to inspect what's goin' on during plugin load

--- tests/plugin.py
+++ tests/plugin.py
@@ -3,13 +3,15 @@
 # License: MIT
 #
 # pylint: disable=missing-docstring,invalid-name
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import os.path
 import os
+import pkg_resources
 import unittest
 
 import anyconfig
+import anyconfig.backends
 
 from tests.common import _bytes, dicts_equal
 
@@ -22,7 +24,20 @@ class Test(unittest.TestCase):
     conf_path = os.path.join(_CURDIR, "0.cbor")
 
     def test_20_load(self):
-        cnf = anyconfig.load(self.conf_path)
+        try:
+            cnf = anyconfig.load(self.conf_path)
+        except anyconfig.UnknownFileTypeError:
+            for entry in pkg_resources.iter_entry_points("anyconfig_backends"):
+                print("%r" % entry)
+                psr = entry.load()
+                print("%r" % psr)
+                print("type=%r, exts=%r" % (psr.type(), psr.extensions()))
+
+            print("all types=%r" % anyconfig.list_types())
+            # psr = anyconfig.backends.find_by_type("cbor")
+            # print("%r, exts=%r" % (psr, psr.extensions()))
+            raise
+
         ref = {_bytes('a'): 0,
                _bytes('b'): _bytes('bbb'),
                _bytes('c'): 5,
