4725498066b5839cfdb2b1420fecb2bb06825691
Satoru SATOH <satoru.satoh@gmail.com>
2018-07-03 02:10:07 +0900

    change: restructure test cases again
    restructure test cases again and revert previous relocations and renames
    in tests/ to simplify them. tests/backend/*.py were moved into tests/
    again with renames if necessary. tests/common.py was copied from
    python-anyconfig/tests/.
    
    unfortunately, plugin test cases still fails and I'll continue working
    on fixing that.

--- tests/cbor2.py
+++ tests/cbor2.py
@@ -6,7 +6,7 @@
 from __future__ import absolute_import
 
 import anyconfig_cbor2_backend.cbor2 as TT
-import tests.common as TBC
+import tests.base as TBC
 
 
 class HasParserTrait(TBC.HasParserTrait):
