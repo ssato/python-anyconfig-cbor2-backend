98ceb05bf23167354df156d98b489ebed76cdcd8
Satoru SATOH <satoru.satoh@gmail.com>
2018-12-31 16:24:36 +0900

    fix: force load of plugins first and cleanup verbose code

--- tests/plugin.py
+++ tests/plugin.py
@@ -6,12 +6,9 @@
 from __future__ import absolute_import, print_function
 
 import os.path
-import os
-import pkg_resources
 import unittest
 
 import anyconfig
-import anyconfig.backends
 
 from tests.common import to_bytes as _b, dicts_equal
 
@@ -25,17 +22,12 @@ class Test(unittest.TestCase):
 
     def test_20_load(self):
         try:
+            anyconfig.api.load_plugins()
             cnf = anyconfig.load(self.conf_path)
         except anyconfig.UnknownFileTypeError:
-            for entry in pkg_resources.iter_entry_points("anyconfig_backends"):
-                print("%r" % entry)
-                psr = entry.load()
-                print("%r" % psr)
-                print("type=%r, exts=%r" % (psr.type(), psr.extensions()))
-
-            print("all types=%r" % anyconfig.list_types())
-            # psr = anyconfig.backends.find_by_type("cbor")
-            # print("%r, exts=%r" % (psr, psr.extensions()))
+            for psr in anyconfig.api.Parsers.list():
+                print("%r: type=%r, exts=%r" % (psr, psr.type(),
+                                                psr.extensions()))
             raise
 
         ref = {_b('a'): 0,
