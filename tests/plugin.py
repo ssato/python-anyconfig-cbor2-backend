54f82b688a2abbed46c8acee50173ab741c14dfe
Satoru SATOH <satoru.satoh@gmail.com>
2019-01-02 01:13:59 +0900

    enhancement: add a test case of explicit use

--- tests/plugin.py
+++ tests/plugin.py
@@ -14,22 +14,33 @@ from tests.common import to_bytes as _b, dicts_equal
 
 
 _CURDIR = os.path.dirname(__file__)
+_CID = "cbor2"  # .. seealso:: anyconfig_cbor2_backend/cbor2.py
 
 
 class Test(unittest.TestCase):
 
     conf_path = os.path.join(_CURDIR, "0.cbor")
 
-    def test_20_load(self):
+    def _load_helper(self, **kwargs):
         try:
             anyconfig.api.load_plugins()
-            cnf = anyconfig.load(self.conf_path)
+            return anyconfig.load(self.conf_path, **kwargs)
         except anyconfig.UnknownFileTypeError:
             for psr in anyconfig.api.Parsers.list():
                 print("%r: type=%r, exts=%r" % (psr, psr.type(),
                                                 psr.extensions()))
             raise
 
+    def test_20_load(self):
+        cnf = self._load_helper()
+        ref = {_b('a'): 0,
+               _b('b'): _b('bbb'),
+               _b('c'): 5,
+               _b('sect0'): {_b('d'): [_b('x'), _b('y'), _b('z')]}}
+        self.assertTrue(dicts_equal(cnf, ref))
+
+    def test_22_load__explicit_use(self):
+        cnf = self._load_helper(ac_parser=_CID)
         ref = {_b('a'): 0,
                _b('b'): _b('bbb'),
                _b('c'): 5,
