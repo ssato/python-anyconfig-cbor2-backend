0890bfc8f7c8112479deb1c87f011738bb259971
Satoru SATOH <satoru.satoh@gmail.com>
2019-09-23 12:08:06 +0900

    enhancement/change: avoid to embed static version strings several times
    avoid to embed version strings more than twice and set it to the value
    of __version__ in \*/__init__.py.
    
    - setup.cfg: find the version value from in \*/__init__.py and allow
      snapshot revisioning at the same time
    - pkg/package.spec.in: parameterized the revision also

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from __future__ import absolute_import
 
+import glob
 import os
+import re
 import setuptools
 import setuptools.command.bdist_rpm
 
 
-VERSION = "0.0.4"
+VERSION = False
+for pyf in glob.glob("*/__init__.py"):
+    matches = [m.groups() for m in (re.match(r'__version__ = "([0-9.]+)"', l)
+                                    for l in open(pyf).readlines()) if m]
+    if matches:
+        VERSION = matches[0][0]
+
+assert VERSION
 
 # For daily snapshot versioning mode:
+RELEASE = "1"
 if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
     import datetime
-    VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
+    RELEASE = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 
 class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
@@ -23,6 +33,9 @@ class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
         """Replace some strings in the RPM SPEC template"""
         if "@VERSION@" in line:
             return line.replace("@VERSION@", VERSION)
+
+        if "@RELEASE@" in line:
+            return line.replace("@RELEASE@", RELEASE)
 
         if "Source0:" in line:  # Dirty hack
             return "Source0: %{pkgname}-%{version}.tar.gz"
